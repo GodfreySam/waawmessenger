df6e927082b3b03890305f54095c380fe8fce2a3
GodfreySam <godfreysam09@gmail.com>
2021-09-03 11:20:12 +0100

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const path = require('path');
-const ejs = require('ejs');
+const port = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+
+const Message = require("./models/Message");
+
+// DB Connection
+mongoose.connect('mongodb://localhost/waawmessenger')
+   .then((dbconnect) => console.log('DB connected successfully ::::::::'))
+   .catch((error) => console.log('DB connection error:', error.message));
 
 const app = express();
 
@@ -12,14 +20,38 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res, next) => {
-   res.render('index');
+app.locals.moment = require('moment');
+
+app.get("/", async (req, res, next) => {
+   try {
+      let allmessages = await Message.find({}).sort({ _id: -1 });
+
+         res.render('index', { messages: allmessages });
+
+   } catch (err) {
+      console.log(err)
+   }
 });
 
-app.get("/about", (req, res, next) => {
-   res.render('');
+app.post("/message/create-message", (req, res, next) => {
+   let { message } = req.body;
+
+   if (!message) {
+     return res.redirect('/');
+   }
+
+   let newMessage = new Message({
+      message
+   });
+
+   newMessage.save()
+      .then((data) => console.log('Message created successfully', data))
+      .catch((error) => console.log('Error creating message', error));
+   
+   res.redirect('/');
+
 });
 
-const port = process.env.PORT || 9000;
+// heroku repo link: https://git.heroku.com/waawmessenger.git
 
 app.listen(port, ()=> console.log(`server running on port:: ${port}`));
